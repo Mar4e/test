987303477c1b05d42efda9349eb1c80e37842141
Mar4e <ctark58@gmail.com>
2020-06-08 19:17:47 +0300

    Это наш первый комит

---
+++ projectJS1/javascript/all.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let person = {
+  name: 'John',
+  age: 26,
+  isMarried: true
+};
+
+let cars = ['Mazda','Mitsubishi','Toyota'];
+
+
+console.log(cars[0]);
+
+let switcher = null;
+
+if (switcher) {
+  console.log('Working..')
+}
+
+switcher = 1
